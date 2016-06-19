88c6b31143d58cdd76b31d931532a88f1e2765ca
Diego Mancilla <anayamd@hotmail.com>
2016-06-19 13:25:24 -0500

    Rediseño de Inicio, Estado y Universidad

--- unsearch/urls.py
+++ unsearch/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.home, name='home'),
 	url(r'^estado/(?P<pk>[0-9]+)/$', views.estado, name='estado'),
+	url(r'^universidad/(?P<pk>[0-9]+)/$', views.universidad, name='universidad'),
 ]
--- unsearch/views.py
+++ unsearch/views.py
@@ -7,6 +7,11 @@ def home(request):
 
 def estado(request, pk):
 	estado = get_object_or_404(Estado, pk=pk)
-	carreras = Carrera.objects.filter(universidad__estado__pk=pk).count
+	num_carreras = Carrera.objects.filter(universidad__estado__pk=pk).count
 	universidades = Universidad.objects.filter(estado__pk=pk)
-	return render(request,'unsearch/estado.html', {'estado':estado, 'carreras':carreras, 'universidades':universidades})
+	return render(request,'unsearch/estado.html', {'estado':estado, 'carreras':num_carreras, 'universidades':universidades})
+
+def universidad(request, pk):
+	universidad = get_object_or_404(Universidad, pk=pk)
+	carreras = Carrera.objects.filter(universidad__pk=pk)
+	return render(request,'unsearch/universidad.html', {'carreras':carreras, 'universidad':universidad})
