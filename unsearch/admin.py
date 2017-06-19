8816e2988b54fba73867deb7472b7649be8bd12e
Diego Said Anaya Mancilla <mancilladev@gmail.com>
2017-06-18 19:05:58 -0500

    Add CSV Exports

--- unsearch/admin.py
+++ unsearch/admin.py
@@ -1,35 +1,43 @@
 from django.contrib import admin
 from django.db.models import Count
 from .models import Carrera, Universidad, Estado, Busqueda
+from .actions import export_as_csv_action
 
 class CarreraInline(admin.TabularInline):
-	model = Carrera
+    model = Carrera
 
 @admin.register(Carrera)
 class CarreraAdmin(admin.ModelAdmin):
-	list_display = ('nombre', 'universidad', 'grado')
-	list_filter = ('universidad__estado__nombre', 'grado')
-	search_fields = ('nombre',)
+    list_display = ('nombre', 'universidad', 'grado')
+    list_filter = ('universidad__estado__nombre', 'grado')
+    search_fields = ('nombre',)
+    actions = [export_as_csv_action("CSV Export", fields=['nombre', 'universidad', 'grado'])]
 
 @admin.register(Universidad)
 class UniversidadAdmin(admin.ModelAdmin):
-	list_display = ('nombre', 'show_carreras_count', 'estado', 'tipo')
-	list_filter = ('estado__nombre', 'tipo')
-	search_fields = ('nombre',)
-	inlines = [
-		CarreraInline,
-	]
-	def get_queryset(self, request):
-		return Universidad.objects.annotate(carreras_count=Count('carreras'))
-	def show_carreras_count(self, inst):
-		return inst.carreras_count
-	show_carreras_count.admin_order_field = 'carreras_count'
+    list_display = ('nombre', 'show_carreras_count', 'estado', 'tipo')
+    list_filter = ('estado__nombre', 'tipo')
+    search_fields = ('nombre',)
+    inlines = [
+        CarreraInline,
+    ]
+    def get_queryset(self, request):
+        return Universidad.objects.annotate(carreras_count=Count('carreras'))
+    def show_carreras_count(self, inst):
+        return inst.carreras_count
+    show_carreras_count.admin_order_field = 'carreras_count'
+    actions = [export_as_csv_action("CSV Export", fields=['estado', 'nombre', 'carreras_count', 'tipo', 'sitio_web'])]
 
 @admin.register(Estado)
 class EstadoAdmin(admin.ModelAdmin):
-	list_display = ('pk', 'nombre', 'municipios', 'imagen')
+    list_display = ('pk', 'nombre', 'municipios', 'show_universidades_count', 'imagen')
+    def get_queryset(self, request):
+        return Estado.objects.annotate(universidades_count=Count('universidades'))
+    def show_universidades_count(self, inst):
+        return inst.universidades_count
+    actions = [export_as_csv_action("CSV Export", fields=['nombre', 'municipios', 'universidades_count'])]
 
 @admin.register(Busqueda)
 class BusquedaAdmin(admin.ModelAdmin):
-	list_display = ('fecha', 'query', 'root', 'estado', 'num_resultados')
-	list_filter = ('estado', 'root')
+    list_display = ('fecha', 'query', 'root', 'estado', 'num_resultados')
+    list_filter = ('estado', 'root')
--- unsearch/views.py
+++ unsearch/views.py
@@ -9,53 +9,54 @@ from django.utils import timezone
 stemmer = SnowballStemmer("spanish")
 
 def registro_de_busquedas(busqueda, raiz, estado, num_universidades):
-	Busqueda.objects.create(estado=estado, query=busqueda, root=raiz, fecha=timezone.now(), num_resultados=num_universidades)
+  Busqueda.objects.create(estado=estado, query=busqueda, root=raiz, fecha=timezone.now(), num_resultados=num_universidades)
 
 def home(request):
-	carreras = Carrera.objects.count()
-	universidades = Universidad.objects.count()
-	return render(request,'unsearch/index.html', {'universidades':universidades, 'carreras':carreras})
+  carreras = Carrera.objects.count()
+  universidades = Universidad.objects.count()
+  return render(request,'unsearch/index.html', {'universidades':universidades, 'carreras':carreras})
 
 def estado(request, pk):
-	estado = get_object_or_404(Estado, pk=pk)
-	num_carreras = Carrera.objects.filter(universidad__estado__pk=pk).count
-	universidades = Universidad.objects.filter(estado__pk=pk)
-	return render(request,'unsearch/estado.html', {'estado':estado, 'carreras':num_carreras, 'universidades':universidades})
+  estado = get_object_or_404(Estado, pk=pk)
+  num_carreras = Carrera.objects.filter(universidad__estado__pk=pk).count
+  universidades = Universidad.objects.filter(estado__pk=pk)
+  return render(request,'unsearch/estado.html', {'estado':estado, 'carreras':num_carreras, 'universidades':universidades})
 
 def estado_buscar(request, pk):
-	if request.method == 'POST':
-		_s = request.POST.get('busqueda').strip()
-		if _s == '':
-			return redirect('unsearch:estado', pk=pk)
-		estado = get_object_or_404(Estado, pk=pk)
-		root = stemmer.stem(_s)
-		carreras = Carrera.objects.distinct().filter( Q(universidad__estado__pk=pk) & (Q(nombre__icontains=root) | Q(nombre__icontains=_s)) ) #.annotate(matches=F('carreras__nombre'))
-		registro_de_busquedas(_s, root, estado, len(carreras))
-		return render(request,'unsearch/busqueda_estado.html', {'estado':estado, 'carreras':carreras, 'busqueda':_s, 'num_resultados':len(carreras)})
-	else:
-		return redirect('unsearch:estado', pk=pk)
+  if request.method == 'POST':
+    _s = request.POST.get('busqueda').strip()
+    if _s == '':
+      return redirect('unsearch:estado', pk=pk)
+    estado = get_object_or_404(Estado, pk=pk)
+    root = stemmer.stem(_s)
+    carreras = Carrera.objects.distinct().filter( Q(universidad__estado__pk=pk) & (Q(nombre__icontains=root) | Q(nombre__icontains=_s)) ) #.annotate(matches=F('carreras__nombre'))
+    registro_de_busquedas(_s, root, estado, len(carreras))
+    return render(request,'unsearch/busqueda_estado.html', {'estado':estado, 'carreras':carreras, 'busqueda':_s, 'num_resultados':len(carreras)})
+  else:
+    return redirect('unsearch:estado', pk=pk)
 
 def universidad(request, pk):
-	universidad = get_object_or_404(Universidad, pk=pk)
-	carreras = Carrera.objects.filter(universidad__pk=pk).order_by('grado')
-	return render(request,'unsearch/universidad.html', {'carreras':carreras, 'universidad':universidad})
+  universidad = get_object_or_404(Universidad, pk=pk)
+  carreras = Carrera.objects.filter(universidad__pk=pk).order_by('grado')
+  return render(request,'unsearch/universidad.html', {'carreras':carreras, 'universidad':universidad})
 
 def buscador_nacional(request):
-	if request.method == 'POST':
-		_s = request.POST.get('busqueda').strip()
-		if _s == '':
-			return redirect('unsearch:home')
-		estado = get_object_or_404(Estado, pk=127)
-		root = stemmer.stem(_s)
-		carreras = Carrera.objects.distinct().filter( Q(nombre__icontains=root) | Q(nombre__icontains=_s) ).order_by('universidad__estado__nombre')
-		tipos = sorted([x['universidad__tipo'] for x in carreras.order_by('universidad__tipo').values('universidad__tipo')])
-		registro_de_busquedas(_s, root, estado, len(carreras))
-		return render(request,'unsearch/busqueda_nacional.html', {'carreras':carreras, 'busqueda':_s, 'tipos':tipos, 'num_resultados':len(carreras)})
-	else:
-		return redirect('unsearch:home')
+  if request.method == 'POST':
+    _s = request.POST.get('busqueda').strip()
+    if _s == '':
+      return redirect('unsearch:home')
+    estado = get_object_or_404(Estado, pk=127)
+    root = stemmer.stem(_s)
+    carreras = Carrera.objects.distinct().filter( Q(nombre__icontains=root) | Q(nombre__icontains=_s) ).order_by('universidad__estado__nombre')
+    tipos = sorted([x['universidad__tipo'] for x in carreras.order_by('universidad__tipo').values('universidad__tipo')])
+    registro_de_busquedas(_s, root, estado, len(carreras))
+    print('Ready to go')
+    return render(request,'unsearch/busqueda_nacional.html', {'carreras':carreras, 'busqueda':_s, 'tipos':tipos, 'num_resultados':len(carreras)})
+  else:
+    return redirect('unsearch:home')
 
 def test(request):
-	return render(request, 'unsearch/test.html')
+  return render(request, 'unsearch/test.html')
 
 def contacto(request):
-	return render(request, 'unsearch/contacto.html')
+  return render(request, 'unsearch/contacto.html')
